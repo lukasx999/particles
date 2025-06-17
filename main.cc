948568bbd9069c9b82be4962bb8526d32843cd39
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-06-18 00:12:06 +0200

    particle collisions + random particle generation

--- main.cc
+++ main.cc
@@ -1,37 +1,62 @@
 #include <cassert>
 #include <print>
 #include <format>
+#include <random>
+#include <span>
 
 #include <raylib.h>
 #include <raymath.h>
 
-#define DEBUG
+#undef DEBUG
+#define NO_GRAVITY
 
 static constexpr auto WIDTH = 1600;
 static constexpr auto HEIGHT = 900;
 
 enum class Direction { Left, Right, Up, Down };
 
+[[nodiscard]] static float rng() {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    auto num = rng() / static_cast<float>(rng.max());
+    assert(num <= 1.0f);
+    assert(num >= 0.0f);
+    return num;
+}
+
 class Particle {
     Vector2 m_pos;
     Vector2 m_vel { 0, 0 };
     Vector2 m_acc { 0, 0 };
     Vector2 m_gravity { 0, 500 };
+    const Color m_color;
     static constexpr float m_radius = 50;
 
 public:
-    Particle(Vector2 pos)
-    : m_pos(pos)
+    Particle(Vector2 pos, Vector2 vel, Color color)
+        : m_pos(pos)
+        , m_vel(vel)
+        , m_color(color)
     { }
+
+    static Particle random() {
+        std::array colors {
+            RED, BLUE, GREEN
+        };
+        size_t idx = rng() * colors.size();
+        return Particle({ rng()*WIDTH, rng()*HEIGHT }, { rng(), rng() }, colors[idx]);
+    }
 
     void update(float dt) {
         m_pos = Vector2Add(m_pos, Vector2Scale(m_vel, dt));
         m_vel = Vector2Add(m_vel, Vector2Scale(m_acc, dt));
-        // m_vel = Vector2Add(m_vel, Vector2Scale(m_gravity, dt));
+        #ifndef NO_GRAVITY
+        m_vel = Vector2Add(m_vel, Vector2Scale(m_gravity, dt));
+        #endif // NO_GRAVITY
     }
 
     void draw() const {
-        DrawCircleV(m_pos, m_radius, BLUE);
+        DrawCircleV(m_pos, m_radius, m_color);
 
         #ifdef DEBUG
         float line_size = 3;
@@ -44,29 +69,44 @@ public:
         #endif // DEBUG
     }
 
-    void resolve_collisions() {
+    void resolve_collisions_others(std::span<Particle> others) {
+
+        for (auto &other : others) {
+            if (&other == this) return;
+
+            // float dist = Vector2Distance(m_pos, other.m_pos) / 2;
+            auto axis = Vector2Subtract(other.m_pos, m_pos);
+            DrawLineEx(m_pos, Vector2Add(m_pos, axis), 1, PURPLE);
+
+            if (CheckCollisionCircles(m_pos, m_radius, other.m_pos, other.m_radius)) {
+                // m_pos = Vector2Add(m_pos, Vector2Scale(Vector2Normalize(axis), dist));
+                // other.m_pos = Vector2Add(other.m_pos, Vector2Scale(axis, dist));
+                m_vel *= -1;
+                other.m_vel *= -1;
+            }
+
+        }
+
+    }
+
+    void resolve_collisions_wall() {
         assert(m_pos.x < WIDTH);
         assert(m_pos.y < HEIGHT);
         assert(m_pos.y > 0);
         assert(m_pos.x > 0);
 
         float damp_factor = 0.95;
+        bool down  = m_pos.y + m_radius >= HEIGHT;
+        bool up    = m_pos.y - m_radius <= 0;
+        bool right = m_pos.x + m_radius >= WIDTH;
+        bool left  = m_pos.x - m_radius <= 0;
 
-        if (m_pos.y-m_radius <= 0) {
-            m_vel.y *= -1;
+        if (up || down) {
+            m_vel.y *= -damp_factor;
         }
 
-        if (m_pos.y+m_radius >= HEIGHT) {
-            m_vel.y *= -1;
-        }
-
-        if (m_pos.x+m_radius >= WIDTH) {
+        if (right || left) {
             m_vel.x *= -damp_factor;
-            // m_acc.x *= 0.9;
-        }
-
-        if (m_pos.x-m_radius <= 0) {
-            m_vel.x *= -1;
         }
 
     }
@@ -93,7 +133,6 @@ public:
         }
     }
 
-
 };
 
 int main() {
@@ -102,31 +141,52 @@ int main() {
     SetTargetFPS(60);
     InitWindow(WIDTH, HEIGHT, "particles");
 
-    Particle p({ WIDTH/2.0f, HEIGHT/2.0f });
+    float vel = 300;
+
+    std::array particles {
+        // Particle({ 100, HEIGHT/2.0f }, { vel, 0 }, RED),
+        // Particle({ WIDTH-100, HEIGHT/2.0f }, { -vel, 0 }, BLUE),
+        Particle::random(),
+        Particle::random(),
+        Particle::random(),
+        Particle::random(),
+        Particle::random(),
+        Particle::random(),
+    };
+
+    // std::vector<Particle> particles;
+    // for (int i=0; i < 5; ++i) {
+    //     particles.push_back(Particle::random());
+    // }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
             ClearBackground(BLACK);
 
-            if (IsKeyPressed(KEY_RIGHT))
-                p.apply_force(Direction::Right);
+            for (auto &p : particles) {
 
-            if (IsKeyPressed(KEY_LEFT))
-                p.apply_force(Direction::Left);
+                if (IsKeyPressed(KEY_RIGHT))
+                    p.apply_force(Direction::Right);
 
-            if (IsKeyPressed(KEY_UP))
-                p.apply_force(Direction::Up);
+                if (IsKeyPressed(KEY_LEFT))
+                    p.apply_force(Direction::Left);
 
-            if (IsKeyPressed(KEY_DOWN))
-                p.apply_force(Direction::Down);
+                if (IsKeyPressed(KEY_UP))
+                    p.apply_force(Direction::Up);
 
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-                p.apply_force_absolute(GetMousePosition());
+                if (IsKeyPressed(KEY_DOWN))
+                    p.apply_force(Direction::Down);
 
-            p.resolve_collisions();
-            p.update(GetFrameTime());
-            p.draw();
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                    p.apply_force_absolute(GetMousePosition());
+
+                p.resolve_collisions_others(particles);
+                p.resolve_collisions_wall();
+                p.update(GetFrameTime());
+                p.draw();
+            }
+
         }
         EndDrawing();
     }
